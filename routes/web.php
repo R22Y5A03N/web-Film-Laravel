86f8e5814ed0318df27302df07cc355fb847a14d
R22Y5A03N <ryanahmad220503@gmail.com>
2025-01-20 12:24:55 +0700

    menambahkan route untuk register

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,8 @@ use App\Http\Controllers\AuthController;
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login')->middleware('guest');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Halaman register
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register')->middleware('guest');
+Route::post('/register', [AuthController::class, 'register']);
+
