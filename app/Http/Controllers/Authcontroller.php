da1699caa28e701c729ab0340dba6851d1e51318
R22Y5A03N <ryanahmad220503@gmail.com>
2025-01-20 12:06:50 +0700

    menambahkan fungsi register

--- app/Http/Controllers/Authcontroller.php
+++ app/Http/Controllers/Authcontroller.php
@@ -37,4 +37,25 @@ class AuthController extends Controller {
         Auth::logout();
         return redirect('/login');
     }
+    public function showRegisterForm()
+    {
+        return view('Auth.register');
+    }
+
+    // Proses register
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        $data['password'] = Hash::make($data['password']);
+        $data['role'] = 'user'; // Default role untuk pendaftaran baru
+
+        User::create($data);
+
+        return redirect('/login')->with('success', 'Registrasi berhasil. Silakan login.');
+    }
 }
